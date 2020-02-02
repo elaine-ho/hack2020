0d7195e017bc0f5a444e334d51c43173ef5fd69d
Elaine Ho <elaine48e@gmail.com>
2020-02-01 22:01:45 -0800

    she talks more

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,28 @@
 #include <gtk/gtk.h>
 #include <cairo.h>
-
+#include <stdlib.h> 
+#include <string>
 
 int x;
 int max_x;
 int y;
+
+int dirx = 0;
+int target_x = 0;
+
+const char* right[] = { "Images/4.png", "Images/5.png", "Images/6.png" };
+const char* left[] = { "Images/10.png", "Images/11.png", "Images/12.png" };
+int img_pos = -1;
+int img_pos_adder = 1;
+
+int animation_counter = 0;
+
+bool text_showing = false;
+int text_counter = 200;
+const char* text[] = { "u got this", "so proud of u", "love you hehe", "ur so cool", "uwu", "dont give up", "owo", "qq keep going", "keep fighting", "ur amazing", "u work so hard", "stay strong", "focus!" };
+int text_n = 13;
+int text_num;
+
 
 gboolean on_draw(GtkWidget* widget, GdkEventExpose* event, gpointer data)
 {
@@ -14,18 +32,63 @@ gboolean on_draw(GtkWidget* widget, GdkEventExpose* event, gpointer data)
     drawingContext = gdk_window_begin_draw_frame(window, cairoRegion);
 
     cairo_t* cr = gdk_drawing_context_get_cairo_context(drawingContext);
-    cairo_set_source_rgba(cr, 1.0, 1.0, 1.0, 0.0);
 
+    if (text_showing) {
+        cairo_set_source_rgb(cr, 230, 230, 230);
+        cairo_select_font_face(cr, "Purisa", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
+        cairo_set_font_size(cr, 13);
+
+        int y_loc = y - 55;
+        int x_loc = x * 2;
+
+        cairo_move_to(cr, x_loc, y_loc);
+        cairo_show_text(cr, text[text_num]);
+    }
     int              w, h;
     cairo_surface_t* image;
 
-    image = cairo_image_surface_create_from_png("Images/8.png");
+    char filename[15];
+    
+    if (dirx == 0) {
+        strcpy_s(filename, "Images/8.png");
+    }
+    else{
+        if (img_pos == -1) {
+            img_pos = 1;
+        }
+
+        if (dirx > 0) strcpy_s(filename, right[img_pos]);
+        else strcpy_s(filename, left[img_pos]);
+
+        if (animation_counter == 5) {
+            if (img_pos + img_pos_adder >= 3) {
+                img_pos_adder = -1;
+            }
+            if (img_pos + img_pos_adder < 0) {
+                img_pos_adder = 1;
+            }
+            img_pos = img_pos + img_pos_adder;
+            animation_counter = 0;
+        }
+        else {
+            animation_counter++;
+        }
+    }
+
+    image = cairo_image_surface_create_from_png(filename);
     w = cairo_image_surface_get_width(image);
     h = cairo_image_surface_get_height(image);
 
     cairo_scale(cr, 2, 2);
 
     int y_pos = y / 2 - h;
+
+    x = x + dirx;
+
+    if (x == target_x) {
+        dirx = 0;
+        img_pos = -1;
+    }
 
     cairo_set_source_surface(cr, image, x, y_pos);
     cairo_paint(cr);
@@ -39,6 +102,45 @@ gboolean on_draw(GtkWidget* widget, GdkEventExpose* event, gpointer data)
 
 }
 
+static gboolean time_handler(GtkWidget* widget)
+{
+    if ((GtkWindow*)widget == NULL) return FALSE;
+
+    time_t curtime;
+    struct tm loctime;
+
+    curtime = time(NULL);
+    localtime_s(&loctime, &curtime);
+
+    if (dirx == 0) {
+        if (rand() % 150 == 0) {
+            target_x = rand() % (max_x / 2);
+            if (target_x > x) {
+                dirx = 1;
+            }
+            else if (target_x < x) {
+                dirx = -1;
+            }
+        }
+    }
+
+    if (text_showing == false) {
+        if (rand() % 200 == 0) {
+            text_showing = true;
+            text_counter = 200;
+            text_num = rand() % text_n;
+        }
+    }
+    else {
+        text_counter--;
+        if (text_counter == 0) {
+            text_showing = false;
+        }
+    }
+
+    gtk_widget_queue_draw(widget);
+    return TRUE;
+}
 
 
 int main(int argc, char* argv[])
@@ -50,10 +152,11 @@ int main(int argc, char* argv[])
 
     y = workarea.height;
     x = 30;
-    max_x = workarea.width;
+    max_x = workarea.width - 25;
 
     GtkWindow* window;
     window = (GtkWindow*)gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(window, "i love you");
     gtk_window_set_default_size(window, 30, 140);
     gtk_window_set_decorated(window, false);
     g_signal_connect(window, "destroy", gtk_main_quit, NULL);
@@ -71,6 +174,8 @@ int main(int argc, char* argv[])
     gtk_window_set_keep_above((GtkWindow*)window, true);
 
 
+    g_timeout_add(33, (GSourceFunc)time_handler, (gpointer)window);
+    time_handler((GtkWidget*)window);
 
 
     gtk_main();
