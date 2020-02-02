f349d89f20e6aef03e3d930c395c58c77f185917
Elaine Ho <elaine48e@gmail.com>
2020-02-01 19:56:04 -0800

    moved character to better position and maximized screen

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,11 @@
 #include <gtk/gtk.h>
 #include <cairo.h>
 
+
+int x;
+int max_x;
+int y;
+
 gboolean on_draw(GtkWidget* widget, GdkEventExpose* event, gpointer data)
 {
     GdkWindow* window = gtk_widget_get_window(widget);
@@ -14,13 +19,15 @@ gboolean on_draw(GtkWidget* widget, GdkEventExpose* event, gpointer data)
     int              w, h;
     cairo_surface_t* image;
 
-    image = cairo_image_surface_create_from_png("Images/ms.png");
+    image = cairo_image_surface_create_from_png("Images/8.png");
     w = cairo_image_surface_get_width(image);
     h = cairo_image_surface_get_height(image);
 
-    cairo_scale(cr, .3, .3);
+    cairo_scale(cr, 2, 2);
 
-    cairo_set_source_surface(cr, image, 0, 0);
+    int y_pos = y / 2 - h;
+
+    cairo_set_source_surface(cr, image, x, y_pos);
     cairo_paint(cr);
     cairo_surface_destroy(image);
 
@@ -38,6 +45,13 @@ int main(int argc, char* argv[])
 {
     gtk_init(&argc, &argv);
 
+    GdkRectangle workarea = { 0 };
+    gdk_monitor_get_workarea(gdk_display_get_primary_monitor(gdk_display_get_default()), &workarea);
+
+    y = workarea.height;
+    x = 30;
+    max_x = workarea.width;
+
     GtkWindow* window;
     window = (GtkWindow*)gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_default_size(window, 30, 140);
@@ -52,8 +66,13 @@ int main(int argc, char* argv[])
 
 
     gtk_widget_show_all((GtkWidget*)window);
-    //gtk_window_maximize(GTK_WINDOW(window));
+
+    gtk_window_maximize(GTK_WINDOW(window));
     gtk_window_set_keep_above((GtkWindow*)window, true);
+
+
+
+
     gtk_main();
 
     return 0;
