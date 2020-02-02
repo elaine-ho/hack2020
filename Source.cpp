f09b4880bdc54b53e1a9f74d6adb964779224719
Elaine Ho <elaine48e@gmail.com>
2020-02-01 18:07:47 -0800

    Added sprite on top

--- Source.cpp
+++ Source.cpp
@@ -1,33 +1,57 @@
 #include <gtk/gtk.h>
+#include <cairo.h>
+
+gboolean on_draw(GtkWidget* widget, GdkEventExpose* event, gpointer data)
+{
+    GdkWindow* window = gtk_widget_get_window(widget);
+    cairo_region_t* cairoRegion = cairo_region_create();
+    GdkDrawingContext* drawingContext;
+    drawingContext = gdk_window_begin_draw_frame(window, cairoRegion);
+
+    cairo_t* cr = gdk_drawing_context_get_cairo_context(drawingContext);
+
+    int              w, h;
+    cairo_surface_t* image;
+
+    image = cairo_image_surface_create_from_png("Images/ms.png");
+    w = cairo_image_surface_get_width(image);
+    h = cairo_image_surface_get_height(image);
+
+    cairo_scale(cr, .3, .3);
+
+    cairo_set_source_surface(cr, image, 0, 0);
+    cairo_paint(cr);
+    cairo_surface_destroy(image);
+
+    gdk_window_end_draw_frame(window, drawingContext);
+
+    cairo_region_destroy(cairoRegion);
+
+    return FALSE;
+
+}
+
+
 
 int main(int argc, char* argv[])
 {
-    GtkWidget* window;
-    GtkWidget* layout;
-    GtkWidget* image;
-
     gtk_init(&argc, &argv);
 
-    GdkRectangle workarea = { 0 };
-    gdk_monitor_get_workarea(gdk_display_get_primary_monitor(gdk_display_get_default()), &workarea);
-    
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_default_size(GTK_WINDOW(window), workarea.width, workarea.height);
-    //gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-
-    //gtk_window_fullscreen(GTK_WINDOW(window));
-
-    layout = gtk_layout_new(NULL, NULL);
-    gtk_container_add(GTK_CONTAINER(window), layout);
-    gtk_widget_show(layout);
-
-    image = gtk_image_new_from_file("Images/petester.png");
-    gtk_layout_put(GTK_LAYOUT(layout), image, 0, 0);
+    GtkWindow* window;
+    window = (GtkWindow*)gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_decorated(GTK_WINDOW(window), false);
+    g_signal_connect(window, "destroy", gtk_main_quit, NULL);
+       
+    GtkDrawingArea* drawingArea;
+    drawingArea = (GtkDrawingArea*)gtk_drawing_area_new();
+    gtk_container_add(GTK_CONTAINER(window), (GtkWidget*)drawingArea);
+    g_signal_connect((GtkWidget*)drawingArea, "draw", G_CALLBACK(on_draw), NULL);
 
 
-    g_signal_connect_swapped(G_OBJECT(window), "destroy",
-    G_CALLBACK(gtk_main_quit), NULL);
-    gtk_widget_show_all(window);
+    gtk_widget_show_all((GtkWidget*)window);
+    //gtk_window_maximize(GTK_WINDOW(window));
+    gtk_window_set_keep_above((GtkWindow*)window, true);
     gtk_main();
+
     return 0;
 }
